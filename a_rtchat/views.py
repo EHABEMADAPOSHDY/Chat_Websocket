cdb267ae715f2d681c08cd3c520992b006f78d16
Ehab Emad <ehabemadaboshady@gmail.com>
2026-08-30 08:20:55 +0300

    Deploy
        new file:   manage.py                                                   #       new file:   pyproject.toml
    Type  :qa  and press <Enter> to exit Vim

---
+++ a_rtchat/views.py
@@ -0,0 +1,38 @@
+from django.shortcuts import render , get_object_or_404 , redirect
+from django.contrib.auth.decorators import login_required
+from .froms import *
+from .models import *
+
+@login_required
+def chat_view(request):
+    chat_group = get_object_or_404(ChatGroup,group_name='public-chat')
+    chat_messages = chat_group.chat_messages.all()[:30]
+    if request.htmx:
+        form = ChatmessageCreateForm(request.POST)
+        if form.is_valid():
+            message = form.save(commit=False)
+            message.author = request.user
+            message.group = chat_group
+            message.save()
+
+            context = {
+                'message': message,
+                'user': request.user
+            }
+
+            return render(
+                request,
+                'a_rtchat/partials/chat_message_p.html',
+                context
+            )
+    else:
+        form = ChatmessageCreateForm()
+
+    return render(
+        request,
+        'a_rtchat/chat.html',
+        {
+            'chat_messages': chat_messages,
+            'form': form
+        }
+    )
